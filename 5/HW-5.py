36e1b6c1a8fe5e04916804c5ae02cb576cfd6a87
SergeyGitAccount <l_w_d@i.ua>
2021-12-26 18:36:29 +0200

    Initial commit

--- 5/HW-5.py
+++ 5/HW-5.py
@@ -1,30 +1,30 @@
 # 1.Написать функцию, принимающую один аргумент.
 # Функция должна вывести на экран (c помощью print) тип данных этого аргумента (type)
 
-# def translater_of_type(value):
-#     print(type(value))
-#
-# translater_of_type("sdfgh")
+def translater_of_type(value):
+    print(type(value))
+
+translater_of_type("sdfgh")
 
 # 2. Написать функцию, принимающую два аргумента. Функция должна
 # - если оба аргумента относятся к числовым типам (int, float) - вернуть их произведение
 # - если к строкам - соединить в одну строку и вернуть
 # - если первый строка, а второй нет - вернуть словарь (dict), в котором ключ - первый аргумент, значение - второй
 # в любом другом случае вернуть кортеж (tuple) из аргументов
-#
-# def calculate_of_type(valueFirst,valueSecond):
-#     if type(valueFirst) == int and type(valueSecond) == int:
-#         return  valueFirst * valueSecond
-#     elif type(valueFirst) == str and type(valueSecond) == str:
-#         return valueFirst + valueSecond
-#     elif type(valueFirst) == str and type(valueSecond) != str:
-#         return ({valueFirst:valueSecond})
-#     else:
-#         return (valueFirst,valueSecond)
-#
-# res = calculate_of_type(1,"2")
-# print(type(res))
-# print(res)
+
+def calculate_of_type(valueFirst,valueSecond):
+    if type(valueFirst) == int and type(valueSecond) == int:
+        return  valueFirst * valueSecond
+    elif type(valueFirst) == str and type(valueSecond) == str:
+        return valueFirst + valueSecond
+    elif type(valueFirst) == str and type(valueSecond) != str:
+        return ({valueFirst:valueSecond})
+    else:
+        return (valueFirst,valueSecond)
+
+res = calculate_of_type(1,"2")
+print(type(res))
+print(res)
 
 
 # Дан словарь продавцов и цен на какой то товар у разных продавцов
@@ -32,17 +32,17 @@
 # Написать функцию возвращающую список имен продавцов, чьи цены попадают в определенный диапазон.
 # Функция должна принимать словарь, начало и конец диапазона и возвращать список имен.
 
-# def price_value (one,two,*dct):
-#     if type(dct) == dict:
-#         return
-#         dct = {}
-#     for x in dct:
-#         for i,j in x.items():
-#             if j >= one and j <= two:
-#                 print(i, j)
-#
-# price_value (30,39,{ 'citrus': 47.999, 'istudio': 42.999, 'moyo': 49.999, 'royal-service': 37.245,
-# 'buy.ua': 38.324, 'g-store': 37.166, 'ipartner': 38.988, 'sota': 37.720 })
+def price_value (one,two,*dct):
+    if type(dct) == dict:
+        return
+        dct = {}
+    for x in dct:
+        for i,j in x.items():
+            if j >= one and j <= two:
+                print(i, j)
+
+price_value (30,39,{ 'citrus': 47.999, 'istudio': 42.999, 'moyo': 49.999, 'royal-service': 37.245,
+'buy.ua': 38.324, 'g-store': 37.166, 'ipartner': 38.988, 'sota': 37.720 })
 
 
 
@@ -53,19 +53,19 @@
 
 
 
-# def compressed (word):
-#   letter_dict = {}
-#   count = 0
-#   for letter in word:
-#     if letter not in letter_dict:
-#         count = 1
-#         letter_dict[letter] = count
-#     else:
-#         count += 1
-#         letter_dict[letter] = count
-#   return letter_dict
-#
-# print (compressed("aaaarggh sdfgaaaaaaaaaa"))
+def compressed (word):
+  letter_dict = {}
+  count = 0
+  for letter in word:
+    if letter not in letter_dict:
+        count = 1
+        letter_dict[letter] = count
+    else:
+        count += 1
+        letter_dict[letter] = count
+  return letter_dict
+
+print (compressed("aaaarggh sdfgaaaaaaaaaa"))
 
 
 
