4208253de4f2a2fe0cda5dc98d597075fae1f244
SergeyGitAccount <l_w_d@i.ua>
2021-12-27 21:07:36 +0200

    Initial commit

--- 6/HW-6-2.py
+++ 6/HW-6-2.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 # * Добавить в задание 2 счетчик попыток и диапазон чисел (начало и конец).
 # Пользователь вводит количество попыток, за которые он может угадать число.
 # Пользователь вводит начало и конец диапазона. На каждом шаге угадывания числа сообщайте пользователю сколько попыток у него осталось.
@@ -11,46 +12,57 @@ def enterInPrograme(stringer):
         return False
     else:
         string = input("Would you like to play again please write 'y' or 'n'").strip().lower()
-        enterInPrograme (string)
+        enterInPrograme(string)
 
-def rangeOfDigits ():
+
+def rangeOfDigits():
     try:
         integerFirst = int(input("Please write start of range  "))
         integerSecond = int(input("Please write start of range  "))
-        random = randint(integerFirst, integerSecond )
+        random = randint(integerFirst, integerSecond)
         print(random)
         return random
     except ValueError:
-            print("Please write only number ")
+        print("Please write only number ")
 
-# rangeOfDigits ()
 
-def countOfAttempt ():
+rangeOfDigits ()
+
+def countOfAttempt():
     try:
         numberAttempt = int(input("Please write number of attempts  "))
         return numberAttempt
     except ValueError:
-            print("Please write only number ")
-
-# res = countOfAttempt()
-# print(res)
+        print("Please write only number ")
 
 
-def enterNumberInGame ():
+res = countOfAttempt()
+print(res)
+
+
+
+def enterNumberInGame():
     attempt = countOfAttempt()
     print(f'you have {attempt} attempts')
     while True:
         try:
-
             integer = int(input("Please write number in range 0-1 "))
-            attempt -= 1
-            print(f'you have {attempt} attempt')
             random = rangeOfDigits()
             if integer == random:
                 string = input("Would you like to play again please write 'y' or 'n'").strip().lower()
+                attempt = countOfAttempt()
+                print(f'you have {attempt} attempts')
                 if not enterInPrograme(string):
+                    break
+            else:
+                attempt = attempt - 1
+                print(f'you have {attempt} attempts')
+                if attempt != 0:
+                    continue
+                else:
+                    print("Loser")
                     break
         except ValueError:
             print("Please write number in range 0-1 ")
 
-enterNumberInGame ()
+enterNumberInGame()
