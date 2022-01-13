95273c286097a164d3a74016c9f3fe640a5dfc76
SergeyGitAccount <l_w_d@i.ua>
2022-01-13 16:59:14 +0200

    Initial commit

--- Practice/CourseStepik.py
+++ Practice/CourseStepik.py
@@ -801,3 +801,49 @@ def f(x):
     elif x > 2:
         return (x - 2) ** 2 + 1
 
+
+# Напишите функцию modify_list(l), которая принимает на вход список целых чисел,
+# удаляет из него все нечётные значения, а чётные нацело делит на два.
+# ункция не должна ничего возвращать, требуется только изменение переданного списка, например:
+
+# lst = [1, 2, 3, 4, 5, 6]
+# print(modify_list(lst))  # None
+# print(lst)               # [1, 2, 3]
+# modify_list(lst)
+# print(lst)               # [1]
+#
+# lst = [10, 5, 8, 3]
+# modify_list(lst)
+# print(lst)               # [5, 4]
+# Функция не должна осуществлять ввод/вывод информации.
+
+def modify_list(l):
+    i = len(l)-1
+    while i >= 0:
+        if l[i] % 2 != 0:
+            del l[i]
+        else:
+            l[i] = l[i] // 2
+        i -= 1
+
+
+
+lst = [10, 5, 3]
+i = 0
+while i < len(lst):
+    if lst[i] % 2 != 0:
+        print("lol2")
+        del lst[i]
+    else:
+        i += 1
+print(lst)
+
+
+def modify_list(l):
+    i=0
+    while i<len(l):
+        if l[i]%2 !=0:
+            del (l[i])
+        else:
+            l[i]=l[i]//2
+            i+=1
